e4622adbf9f53d10a536da96788d3dccc242993a
Adrish Maity <adrish.maity@outlook.com>
2019-06-06 12:45:07 +0530

    Final commit, added the solution

--- way-too-long-words.py
+++ way-too-long-words.py
@@ -1,7 +1,16 @@
 def main():
-    pass
+    n = int(input())
 
+    words = []
+    for _ in range(n):
+        words.append(input())
+
+    for word in words:
+        if len(word)>=10:
+            print(word[0]+str(len(word)-2)+word[-1])
+
+        else:
+            print(word)
 
 if __name__ == "__main__"    :
     main()
-    
