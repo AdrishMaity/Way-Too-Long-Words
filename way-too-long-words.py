8bc5d243afec508f72b3ff517b23996b4dd486d7
Adrish Maity <adrish.maity@outlook.com>
2019-06-06 12:51:55 +0530

    Corrected the condition > 10

--- way-too-long-words.py
+++ way-too-long-words.py
@@ -6,7 +6,7 @@ def main():
         words.append(input())
 
     for word in words:
-        if len(word)>=10:
+        if len(word)>10:
             print(word[0]+str(len(word)-2)+word[-1])
 
         else:
